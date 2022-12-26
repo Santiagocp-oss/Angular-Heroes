9cdc2a4f2044fd9d9b65bc185936d99007f3e0d6
Santiago C <rayo1555@gmail.com>
2022-12-26 11:55:49 -0500

    diseño pantalla

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
+interface personaje{
+  nombre:string,
+  poder : number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  nuevo: personaje = {
+    nombre: 'trunks',
+    poder: 14000
+  }
 
+  Agregar(  ){
+    console.log(this.nuevo);
+  }
 
 }
