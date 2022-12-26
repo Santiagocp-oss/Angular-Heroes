034759523bdae92c1b7b8b790a813d6655b6c479
Santiago C <rayo1555@gmail.com>
2022-12-26 16:56:15 -0500

    component personaje

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,13 +11,33 @@ interface personaje{
 })
 export class MainPageComponent {
 
+  personajes: personaje [] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre:'vegeta',
+      poder: 7500
+    }
+  ];
+
   nuevo: personaje = {
-    nombre: 'trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   Agregar(  ){
+    if (this.nuevo.nombre.trim().length === 0){  return;
+    }
     console.log(this.nuevo);
+
+
+    this.personajes.push(this.nuevo)
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
 
 }
