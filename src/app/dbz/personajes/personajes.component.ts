b46a44e53dccd97686970f4f28d2ffc256471ae3
Santiago C <rayo1555@gmail.com>
2023-01-04 14:29:40 -0500

    salida a produccion

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { personaje } from '../Interfaces/dbz.interfaces';
+import { Component } from '@angular/core';
 import { dbzService } from '../services/dbz.service';
 
 @Component({
@@ -7,8 +6,6 @@ import { dbzService } from '../services/dbz.service';
   templateUrl: './personajes.component.html',
 })
 export class PersonajesComponent {
-
- // @Input('data') personajes : personaje[] = [];  
  get personajes(){
   return this.dbzService.personajes;
  }
